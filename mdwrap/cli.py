5e88c44bb96cb209db0674436abaa1228004ad28
Leonard <73912641+leonardcser@users.noreply.github.com>
2023-07-13 10:51:42 +0200

    Improve cli (#3)
    * Add multiple file support to cli
    
    * Add description
    
    * Add version
    
    * Add reccusive glob
    
    * Add ignore flag + format help
    
    * Modularise arg parser
    
    * Add tests for cli
    
    * Use new version api
    
    * Only parse md files
    
    * Use sets instead of checking lists

--- mdwrap/cli.py
+++ mdwrap/cli.py
@@ -1,65 +1,19 @@
-import argparse
-from pathlib import Path
-from typing import List
-
-from mdwrap.common.abstract.abstract_transform import AbstractTransform
 from mdwrap.common.formatter import Formatter
 from mdwrap.md.line_context import LineContext
-from mdwrap.md.transforms.newline_transform import NewlineTransform
-from mdwrap.md.transforms.trailing_whitespace_transform import (
-    TrailingWhitespaceTransform,
-)
-from mdwrap.md.transforms.unwrap_transform import UnwrapTransform
-from mdwrap.md.transforms.wrap_transform import WrapTransform
+from mdwrap.utils.argparse import MDWrapArgparse
 
 
 def cli() -> None:
     """CLI entrypoint."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--print-width",
-        type=int,
-        default=80,
-        help="Maximum width of a line",
-    )
-    parser.add_argument(
-        "--fmt",
-        action="store_true",
-        help="Format the file(s) (basic newline formatting)",
-    )
-    parser.add_argument(
-        "--unwrap",
-        action="store_true",
-        help="Unwrap the file(s)",
-    )
-    parser.add_argument(
-        "--check",
-        action="store_true",
-        help="Check if the file(s) is/are formatted",
-    )
-    parser.add_argument("target", help="File or folder of files to format")
+    args, files, transforms = MDWrapArgparse().parse_args()
 
-    args = parser.parse_args()
-    target = Path(args.target)
-    print_width = args.print_width
-    fmt = args.fmt
-    unwrap = args.unwrap
-    check = args.check
-
-    transforms: List[AbstractTransform] = [UnwrapTransform()]
-
-    if not unwrap:
-        transforms.append(WrapTransform(print_width=print_width))
-    if fmt:
-        transforms.extend([NewlineTransform(), TrailingWhitespaceTransform()])
-
-    files = [target] if target.is_file() else list(target.glob("**/*.md"))
     line_context = LineContext()
     formatter = Formatter(
         line_context=line_context,
         transforms=transforms,
     )
-    if check:
+
+    if args.check:
         print("Checking files...")
         failed = False
         for file in files:
@@ -70,7 +24,7 @@ def cli() -> None:
             print(
                 (
                     "ERROR: Checks failed. The files above are not formatted. "
-                    "(run mdwrap to format them)"
+                    "(hint: run 'mdwrap [targets ...]' to format them)"
                 )
             )
             exit(1)
---
+++ tests/test_cli.py
@@ -0,0 +1,183 @@
+import re
+from pathlib import Path
+
+from mdwrap.utils.argparse import MDWrapArgparse
+
+
+def test_parse_args():
+    """Test parsing arguments."""
+    parser = MDWrapArgparse()
+
+    # Prepare test arguments
+    test_args = [
+        "--print-width",
+        "100",
+        "--fmt",
+        "--unwrap",
+        "--check",
+        "--ignore",
+        ".git",
+        "--ignore-extend",
+        ".git",
+        "tests/integration/paragraph",
+    ]
+
+    # Parse the arguments
+    args, files, transforms = parser.parse_args(test_args)
+
+    # Assertions
+    assert args.print_width == 100
+    assert args.fmt is True
+    assert args.unwrap is True
+    assert args.check is True
+    assert args.ignore == ".git"
+    assert args.ignore_extend == ".git"
+
+    assert set(files) == {
+        Path('tests/integration/paragraph/expected.md'),
+        Path('tests/integration/paragraph/test.md'),
+    }
+    assert len(transforms) > 0
+
+
+def test_parse_args_with_default_values():
+    """Test parsing arguments with default values."""
+    parser = MDWrapArgparse()
+
+    # Prepare test arguments
+    test_args = ["file.md"]
+
+    # Parse the arguments
+    args, files, transforms = parser.parse_args(test_args)
+
+    # Assertions
+    assert args.print_width == 80
+    assert args.fmt is False
+    assert args.unwrap is False
+    assert args.check is False
+    assert args.ignore == (
+        r"\.direnv|\.eggs|\.git|\.hg|\.mypy_cache|\.pytest_cache|\.nox|\.tox|"
+        r"\.venv|venv|\.svn|_build|buck-out|build|dist|__pypackages__"
+    )
+    assert args.ignore_extend is None
+
+    # Empty because file.md does not exist
+    assert files == []
+    assert len(transforms) > 0
+
+
+def test_parse_args_with_ignore_pattern():
+    """Test parsing arguments with ignore pattern."""
+    parser = MDWrapArgparse()
+
+    # Prepare test arguments
+    test_args = ["--ignore", "README.md", "."]
+
+    # Parse the arguments
+    args, files, transforms = parser.parse_args(test_args)
+
+    # Assertions
+    assert args.ignore == "README.md"
+
+    assert all(re.match(args.ignore, str(f)) for f in files) is False
+    assert len(transforms) > 0
+
+
+def test_parse_args_with_ignore_extend():
+    """Test parsing arguments with ignore extend."""
+    parser = MDWrapArgparse()
+
+    # Prepare test arguments
+    test_args = ["--ignore-extend", "README.md", "."]
+
+    # Parse the arguments
+    args, files, transforms = parser.parse_args(test_args)
+
+    # Assertions
+    assert args.ignore_extend == "README.md"
+
+    assert all(re.match(args.ignore_extend, str(f)) for f in files) is False
+    assert len(transforms) > 0
+
+
+def test_parse_args_with_mutiple_targets():
+    """Test parsing arguments with multiple targets."""
+    parser = MDWrapArgparse()
+
+    # Prepare test arguments
+    test_args = ["tests/integration/paragraph", "tests/integration/list"]
+
+    # Parse the arguments
+    _, files, transforms = parser.parse_args(test_args)
+
+    # Assertions
+    assert set(files) == {
+        Path('tests/integration/paragraph/expected.md'),
+        Path('tests/integration/paragraph/test.md'),
+        Path('tests/integration/list/expected.md'),
+        Path('tests/integration/list/test.md'),
+    }
+    assert len(transforms) > 0
+
+
+def test_parse_args_with_mutiple_targets_and_ignore():
+    """Test parsing arguments with multiple targets and ignore."""
+    parser = MDWrapArgparse()
+
+    # Prepare test arguments
+    test_args = [
+        "--ignore",
+        "tests/integration/paragraph",
+        "tests/integration/paragraph",
+        "tests/integration/list",
+    ]
+
+    # Parse the arguments
+    _, files, transforms = parser.parse_args(test_args)
+
+    # Assertions
+    assert set(files) == {
+        Path('tests/integration/list/expected.md'),
+        Path('tests/integration/list/test.md'),
+    }
+    assert len(transforms) > 0
+
+
+def test_parse_args_with_mutiple_targets_and_ignore_extend():
+    """Test parsing arguments with multiple targets and ignore."""
+    parser = MDWrapArgparse()
+
+    # Prepare test arguments
+    test_args = [
+        "--ignore-extend",
+        "tests/integration/paragraph",
+        "tests/integration/paragraph",
+        "tests/integration/list",
+    ]
+
+    # Parse the arguments
+    _, files, transforms = parser.parse_args(test_args)
+
+    # Assertions
+    assert set(files) == {
+        Path('tests/integration/list/expected.md'),
+        Path('tests/integration/list/test.md'),
+    }
+    assert len(transforms) > 0
+
+
+def test_parse_args_only_md_files():
+    """Test parsing arguments with only md files."""
+    parser = MDWrapArgparse()
+
+    # Prepare test arguments
+    test_args = ["pyproject.toml", "tests/integration/paragraph/test.md"]
+
+    # Parse the arguments
+    _, files, transforms = parser.parse_args(test_args)
+
+    # Assertions
+    assert set(files) == {
+        Path('tests/integration/paragraph/test.md'),
+    }
+    assert len(transforms) > 0
